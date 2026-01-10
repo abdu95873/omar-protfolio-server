b76925aaa6e23d0cb9748a6c91e4972078fa145d
abdu95873 <abdu95873@gmail.com>
2026-01-10 12:17:49 +0600

    server 8

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // Customers Query.................................................
 
@@ -495,6 +495,9 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('boss is sitting')
+})
+app.get('/me', (req, res) => {
+  res.send('Avdullah')
 })
 
 app.listen(port, () => {
